d2ce7f15ae6961c3aef61ffb52dabb96ce50a5bc
Guido Josquin <g.josquin+git@gmail.com>
2018-04-04 13:46:02 +0200

    Add wireframe for code to persist subscriptions

--- src/emq_mod_persistence.erl
+++ src/emq_mod_persistence.erl
@@ -15,20 +15,72 @@
 %%--------------------------------------------------------------------
 
 load(Env) ->
-    io:format("*** PLUGIN **** called load()~n", []),
-    emqttd:hook('client.subscribe', fun ?MODULE:on_client_subscribe/4, [Env]),
-    emqttd:hook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/4, [Env]).
+  io:format("*** PLUGIN **** called load()~n", []),
+  initMnesia(Env),
+  emqttd:hook('client.subscribe', fun ?MODULE:on_client_subscribe/4, [Env]),
+  emqttd:hook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/4, [Env]),
+  ClientTopicTables = loadPersistedSubscriptions(),
+  lists:foreach(subscribeClientToTopics, ClientTopicTables).
 
-on_client_subscribe(ClientId, Username, TopicTable, _Env) ->
-    io:format("*** PLUGIN **** called on_client_subscribe()~n", []),
-    {ok, TopicTable}.
+on_client_subscribe(ClientId, Username, TopicTable, Env) ->
+  io:format("*** PLUGIN **** called on_client_subscribe()~n", []),
+  persistSubscription(ClientId, TopicTable),
+  {ok, TopicTable}.
 
-on_client_unsubscribe(ClientId, Username, TopicTable, _Env) ->
-    io:format("*** PLUGIN **** called on_client_unsubscribe()~n", []),
-    {ok, TopicTable}.
+on_client_unsubscribe(ClientId, Username, TopicTable, Env) ->
+  io:format("*** PLUGIN **** called on_client_unsubscribe()~n", []),
+  forgetSubscription(ClientId, TopicTable),
+  {ok, TopicTable}.
 
 unload() ->
-    io:format("*** PLUGIN **** called unload()~n", []),
-    emqttd:unhook('client.subscribe', fun ?MODULE:on_client_subscribe/4),
-    emqttd:unhook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/4).
+  io:format("*** PLUGIN **** called unload()~n", []),
+  emqttd:unhook('client.subscribe', fun ?MODULE:on_client_subscribe/4),
+  emqttd:unhook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/4).
 
+%%--------------------------------------------------------------------
+%% Helpers
+%%--------------------------------------------------------------------
+
+initMnesia(Env) ->
+  ok.
+%%  Copies = disc_copies,
+%%  ok = ekka_mnesia:create_table(mqtt_persisted, [
+%%    {type, set},
+%%    {Copies, [node()]},
+%%    {record_name, mqtt_persisted},
+%%    {attributes, record_info(fields, mqtt_persisted)},
+%%    {storage_properties, [{ets, [compressed]},
+%%      {dets, [{auto_save, 1000}]}]}]),
+%%  ok = ekka_mnesia:copy_table(mqtt_persisted),
+%%  case mnesia:table_info(mqtt_persisted, storage_type) of
+%%    Copies -> ok;
+%%    _      -> {atomic, ok} = mnesia:change_table_copy_type(mqtt_persisted, node(), Copies)
+%%  end.
+
+loadPersistedSubscriptions() ->
+  io:format("*** PLUGIN *** loading persisted subscriptions...~n", []),
+%%  TODO
+  PersistedSubscriptions = [],
+  io:format("*** PLUGIN *** done loading persisted subscriptions.~n", []),
+  PersistedSubscriptions.
+
+persistSubscription(ClientId, TopicTable) ->
+  io:format("*** PLUGIN *** persisting subscription of ~s to topics ~p...~n", [ClientId, TopicTable]),
+%%  TODO
+%%  mnesia:dirty_write(#mqtt_persisted{topic = Topic, msg = Msg, ts = emqttd_time:now_ms(Ts)})
+  io:format("*** PLUGIN *** done.~n", []).
+
+forgetSubscription(ClientId, TopicTable) ->
+  io:format("*** PLUGIN *** forgetting subscription of ~s to topics ~p...~n", [ClientId, TopicTable]),
+%%  TODO
+%%  mnesia:dirty_delete(mqtt_persisted, {ClientId, TopicTable}),
+  io:format("*** PLUGIN *** done.~n", []).
+
+%% Subscribe a client to a list of topics
+%% TopicTable is a list of {Topic, Qos}
+subscribeClientToTopics(clientTopicTable) ->
+  {ClientId, TopicTable} = clientTopicTable,
+  io:format("*** PLUGIN *** subscribing ~s to topics ~p...~n", [ClientId, TopicTable]),
+%%  TODO
+%%  emqttd_client:subscribe(ClientId, TopicTable),
+  io:format("*** PLUGIN *** done.~n", []).
