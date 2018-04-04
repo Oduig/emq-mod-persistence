8ac63d8f3463db3e41cfa607d75859277597f01d
Guido Josquin <g.josquin+git@gmail.com>
2018-04-04 09:40:29 +0200

    Fix a typo in subscribe/unsubscribe

--- src/emq_mod_persistence.erl
+++ src/emq_mod_persistence.erl
@@ -15,20 +15,20 @@
 %%--------------------------------------------------------------------
 
 load(Env) ->
-    io:format("*** PLUGIN **** called load()", []),
+    io:format("*** PLUGIN **** called load()~n", []),
     emqttd:hook('client.subscribe', fun ?MODULE:on_client_subscribe/4, [Env]),
-    emqttd:hook('client.unsubscribe', fun ?MODULE:on_client_subscribe/4, [Env]).
+    emqttd:hook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/4, [Env]).
 
 on_client_subscribe(ClientId, Username, TopicTable, _Env) ->
-    io:format("*** PLUGIN **** called on_client_subscribe()", []),
+    io:format("*** PLUGIN **** called on_client_subscribe()~n", []),
     {ok, TopicTable}.
 
 on_client_unsubscribe(ClientId, Username, TopicTable, _Env) ->
-    io:format("*** PLUGIN **** called on_client_unsubscribe()", []),
+    io:format("*** PLUGIN **** called on_client_unsubscribe()~n", []),
     {ok, TopicTable}.
 
 unload() ->
-    io:format("*** PLUGIN **** called unload()", []),
+    io:format("*** PLUGIN **** called unload()~n", []),
     emqttd:unhook('client.subscribe', fun ?MODULE:on_client_subscribe/4),
-    emqttd:unhook('client.unsubscribe', fun ?MODULE:on_client_subscribe/4).
+    emqttd:unhook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/4).
 
